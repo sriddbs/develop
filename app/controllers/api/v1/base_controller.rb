1601c3061bea5aba924aeb001c7cd651713c612b
Sridhar B.S <sridhar@jigsawacademy.com>
2016-12-28 18:43:22 +0530

    implemented api token authentication

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::BaseController < ApplicationController
+  API_TOKEN = "da0feda374beb6b3c2dde5e71e68888e"
+
   skip_before_filter :verify_authenticity_token
   skip_before_filter :authenticate_user!
+
+  before_action :authenticate_api_request
+
+  private
+    def authenticate_api_request
+      authenticate_or_request_with_http_token do |token, options|
+        token == API_TOKEN
+      end
+    end
 end
